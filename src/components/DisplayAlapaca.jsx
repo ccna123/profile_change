e0dc5bce4cc237120b3dc93ece37605ed81e4489
THANH <95860402+ccna123@users.noreply.github.com>
2025-02-10 18:29:21 +0900

    Update DisplayAlapaca.jsx

--- src/components/DisplayAlapaca.jsx
+++ src/components/DisplayAlapaca.jsx
@@ -4,37 +4,61 @@ export const DisplayAlapaca = ({ updateStyle }) => {
   return (
     <div id="profile">
       {/* hair */}
-      <img src={updateStyle[0].style} className="absolute -left-3 z-10" />
+      <img
+        alt=""
+        src={updateStyle[0].style}
+        className="absolute -left-3 z-10"
+      />
       {/* end hair */}
 
       {/* ears */}
-      <img src={updateStyle[1].style} className="absolute z-10" />
+      <img alt="" src={updateStyle[1].style} className="absolute z-10" />
       {/* end ears */}
 
       {/* eyes */}
-      <img src={updateStyle[2].style} className="absolute -left-3 z-20" />
+      <img
+        alt=""
+        src={updateStyle[2].style}
+        className="absolute -left-3 z-20"
+      />
       {/* end eyes */}
 
       {/* mouth */}
-      <img src={updateStyle[3].style} className="absolute -left-3 z-20" />
+      <img
+        alt=""
+        src={updateStyle[3].style}
+        className="absolute -left-3 z-20"
+      />
       {/* end mouth */}
 
       {/* neck */}
-      <img src={updateStyle[4].style} className="absolute -left-3 z-10" />
+      <img
+        alt=""
+        src={updateStyle[4].style}
+        className="absolute -left-3 z-10"
+      />
       {/* end neck */}
 
-      <img src="/images/nose/nose.png" className="absolute -left-3 z-10" />
+      <img
+        alt=""
+        src="/images/nose/nose.png"
+        className="absolute -left-3 z-10"
+      />
 
       {/* leg */}
-      <img src={updateStyle[5].style} className="absolute -left-3 z-10" />
+      <img
+        alt=""
+        src={updateStyle[5].style}
+        className="absolute -left-3 z-10"
+      />
       {/* end leg */}
 
       {/* accessories */}
-      <img src={updateStyle[6].style} className="absolute z-10" />
+      <img alt="" src={updateStyle[6].style} className="absolute z-10" />
       {/* end accessories */}
 
       {/* background */}
-      <img src={updateStyle[7].style} className="absolute" />
+      <img alt="" src={updateStyle[7].style} className="absolute" />
       {/* end background */}
     </div>
   );
